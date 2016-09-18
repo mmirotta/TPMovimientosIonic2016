ee1ab15f62a2c233fc78d5c7e4cce5382ccec6f8
matias mirotta <m.mirotta@gmail.com>
2016-09-18 15:16:39 -0300

    Genero proyecto. Detecta movimiento y reproduce sonido correspondiente.

---
+++ www/js/controllers.js
@@ -0,0 +1,160 @@
+angular.module('starter.controllers', ['ngCordova'])
+
+.controller('IngresoCtrl', function($scope, $state) {
+  $scope.usuario = {};
+  $scope.usuario.nombre = "";
+  $scope.Ingresar = function(){
+    var param = JSON.stringify($scope.usuario);
+    $state.go('tab.juego', {usuario:param});
+  };
+})
+
+.controller('JuegoCtrl', function($scope, $state, $stateParams, $cordovaDeviceMotion, $cordovaMedia, $cordovaNativeAudio) {
+  $scope.usuario = JSON.parse($stateParams.usuario);
+  $scope.izquierda = 0;
+  $scope.derecha = 0;
+  $scope.arriba = 0;
+  $scope.abajo = 0;
+  $scope.bocaArriba = 0;
+  $scope.bocaAbajo = 0;
+  $scope.imagen = "img/sinMov.png"; 
+  try
+  {
+    $cordovaNativeAudio
+    .preloadSimple('arriba', 'audios/arriba.mp3')
+    .then(function (msg) {
+      console.log(msg);
+    }, function (error) {
+      alert(error);
+    });
+
+    $cordovaNativeAudio
+    .preloadSimple('abajo', 'audios/abajo.mp3')
+    .then(function (msg) {
+      console.log(msg);
+    }, function (error) {
+      alert(error);
+    });
+
+    $cordovaNativeAudio
+    .preloadSimple('izquierda', 'audios/izquierda.mp3')
+    .then(function (msg) {
+      console.log(msg);
+    }, function (error) {
+      alert(error);
+    });
+
+    $cordovaNativeAudio
+    .preloadSimple('derecha', 'audios/derecha.mp3')
+    .then(function (msg) {
+      console.log(msg);
+    }, function (error) {
+      alert(error);
+    });
+
+    $cordovaNativeAudio
+    .preloadSimple('bocaArriba', 'audios/bocaArriba.mp3')
+    .then(function (msg) {
+      console.log(msg);
+    }, function (error) {
+      alert(error);
+    });
+
+    $cordovaNativeAudio
+    .preloadSimple('bocaAbajo', 'audios/bocaAbajo.mp3')
+    .then(function (msg) {
+      console.log(msg);
+    }, function (error) {
+      alert(error);
+    });
+  }
+  catch(error)
+  {
+    console.log(error);
+  }
+
+
+  $scope.opciones = { frequency: 100 };
+  $scope.ubicacion = {
+      x : null,
+      y : null,
+      z : null,
+      timestamp : null
+  }
+  
+  $scope.watch = $cordovaDeviceMotion.watchAcceleration($scope.opciones);
+
+  $scope.watch.then(
+    null,
+    function(error) {
+    // An error occurred
+    },
+    function(result) {
+      if(result.x > 10){
+        $scope.ubicacion.x = 1;
+        $scope.imagen = "img/izquierda.png"; 
+        if ($scope.izquierda == 0)
+        {
+          $cordovaNativeAudio.play('izquierda');
+          $scope.izquierda = 1;
+        }
+        $scope.derecha = 0;
+        $scope.arriba = 0;
+        $scope.abajo = 0;
+        $scope.bocaArriba = 0;
+        $scope.bocaAbajo = 0;
+
+      }else if(result.x < -10){
+        $scope.ubicacion.x = -1;
+        $scope.imagen = "img/derecha.png"; 
+        if ($scope.derecha == 0)
+        {
+          $cordovaNativeAudio.play('derecha');
+          $scope.derecha = 1;
+        }
+        $scope.izquierda = 0;
+        $scope.arriba = 0;
+        $scope.abajo = 0;
+        $scope.bocaArriba = 0;
+        $scope.bocaAbajo = 0;
+      }else{
+        $scope.ubicacion.x = 0;
+      }
+
+      if(result.y > 10){
+        $scope.ubicacion.y = 1;
+        $scope.imagen = "img/abajo.png"; 
+        $cordovaNativeAudio.play('abajo');        
+      }else if(result.y < -10){
+        $scope.ubicacion.y = -1;
+        $scope.imagen = "img/arriba.png"; 
+        $cordovaNativeAudio.play('arriba');
+      }else{
+        $scope.ubicacion.y = 0;
+      }
+
+      if(result.z > 10){
+        $scope.ubicacion.z = 1;
+        $cordovaNativeAudio.play('bocaArriba');
+      }else if(result.z < -10){
+        $scope.ubicacion.z = -1;
+        $cordovaNativeAudio.play('bocaAbajo');
+      }else{
+        $scope.ubicacion.z = 0;
+      }
+
+      if ($scope.ubicacion.x == 0 && $scope.ubicacion.y == 0 && $scope.ubicacion.z == 1)
+      {
+        $scope.imagen = "img/sinMov.png"; 
+      }
+
+      var timeStamp = result.timestamp;
+  });
+
+
+})
+.controller('GrabarCtrl', function($scope) {
+})
+
+.controller('PerfilCtrl', function($scope) {
+});
